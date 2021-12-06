9060c93856cb1081c2c17c6fc5e06b8338c7de7a
Vaibhav Arora <vaibhavarora2411@gmail.com>
2021-12-06 09:38:43 -0500

    day3 complete

--- day3/programpt2.py
+++ day3/programpt2.py
@@ -1,26 +1,26 @@
 import sys
 import math
 
-def finddecimal(binary):
-    binary1 = binary
-    decimal, i, n = 0, 0, 0
-    while(binary != 0):
-        dec = binary % 10
-        decimal = decimal + dec * pow(2, i)
-        binary = binary//10
-        i += 1
-    return decimal
-
-def findvals(data):
-
-    for i in range(len(data[0])):
-        for vals 
+def filter_data_bitwise(data, filter_by_most_common,bits):
+    filtered = [x for x in data]
+    for bit in reversed(bits):
+        ratio = sum(1 for num in filtered if num & bit) / len(filtered)
+        wanted_bit_value = bit * int((ratio >= 0.5) == filter_by_most_common)
+        filtered = [x for x in filtered if x & bit == wanted_bit_value]
+        if len(filtered) == 1:
+            break
+    return filtered
 
 def main():
     data = []
     while line:=sys.stdin.readline().strip():
         data.append(line)
-    print(findvals(data))
+    n = len(data[0])
+    bits = [2 ** x for x in range(n)]
+    d = [int(line,base=2) for line in data]
+    filtered_by_most_common = filter_data_bitwise(d,True,bits)
+    filtered_by_least_common = filter_data_bitwise(d,False,bits)    
+    print(filtered_by_most_common[0] * filtered_by_least_common[0])
     
 if __name__ == '__main__':
     main()
