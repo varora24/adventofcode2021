16a570148997be2136f77db9c132a3af168c695a
Vaibhav Arora <vaibhavarora2411@gmail.com>
2021-12-10 00:48:01 -0500

    updating repo

---
+++ day4/program.py
@@ -0,0 +1,12 @@
+import sys
+
+def buildCards():
+    d = {}
+    i = 0
+    while sys.stdin.readline()
+def main():
+    numberlist = [int(i) for i in sys.stdin.readline().strip().split(',')]
+    cards = buildCards()
+
+if __name__ == '__main__':
+    main()
